f7ea95684cc48e4361d1753e5e94c99ee12cae3a
Michael Neumann <mneumann@ntecs.de>
2015-09-24 18:04:02 +0200

    Split unicode words

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
+#![feature(slice_concat_ext)]
+
+use unicode_segmentation::UnicodeSegmentation;
+use std::slice::SliceConcatExt;
+
 extern crate unicode_segmentation;
 
 pub fn normalize_tag(tag: &str) -> Result<String, &'static str> {
+
     if tag.is_empty() {
         Err("Tag is empty")
     } else {
-        Ok(tag.to_lowercase())
+        let lc = tag.to_lowercase();
+        let words: Vec<&str> = lc.unicode_words().collect();
+        let joined = words.join("-");
+        Ok(joined)
     }
 }
 
@@ -12,4 +21,7 @@ pub fn normalize_tag(tag: &str) -> Result<String, &'static str> {
 fn test_tag_normalize() {
     assert!(normalize_tag("").is_err());
     assert_eq!(Ok("abc".to_string()), normalize_tag("ABC"));
+    assert_eq!(Ok("abc-this-is-a-tag".to_string()), normalize_tag("ABC this is a tag\""));
+    assert_eq!(Ok("abc-this-is-a-tag".to_string()), normalize_tag("   ABC this     is a tag\""));
+    assert_eq!(Ok("abc-this-is-a-tag".to_string()), normalize_tag("--   ABC this     is a tag--\""));
 }
